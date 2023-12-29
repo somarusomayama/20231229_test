fefd6dff408ee128d88bb1f6afae0b1cde85c3f7
somaru <118579821+somarusomayama@users.noreply.github.com>
2023-12-29 04:37:33 +0000

    create todo list on vanila js

--- dist/index.ac9dc4ba.js
+++ dist/index.ac9dc4ba.js
@@ -576,10 +576,70 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 
 },{}],"aR1JP":[function(require,module,exports) {
 var _stylesCss = require("./styles.css");
-document.getElementById("app").innerHTML = `
-<h1>Hello JavaScript!</h1>
-`;
-console.log("test");
+const textElement = document.getElementById("add-text");
+const completeList = document.getElementById("complete-list");
+const incompleteList = document.getElementById("incomplete-list");
+const onClickAdd = ()=>{
+    const inputText = textElement.value;
+    const completeButton = createCompleteButton();
+    const deleteButton = createDeleteButton();
+    const todoRow = buildTodoTag(inputText, [
+        completeButton,
+        deleteButton
+    ]);
+    incompleteList.appendChild(todoRow);
+    textElement.value = "";
+};
+const onClickCompleteButton = (e)=>{
+    const liTag = e.target.closest("li");
+    e.target.nextElementSibling.remove();
+    e.target.remove();
+    const incompleteButton = createIncompleteButton();
+    liTag.firstElementChild.appendChild(incompleteButton);
+    completeList.appendChild(liTag);
+};
+const onClickDeleteButton = (e)=>{
+    incompleteList.removeChild(e.target.closest("li"));
+};
+const onClickIncompleteButton = (e)=>{
+    const liTag = e.target.closest("li");
+    e.target.remove();
+    const completeButton = createCompleteButton();
+    const deleteButton = createDeleteButton();
+    liTag.firstElementChild.appendChild(completeButton);
+    liTag.firstElementChild.appendChild(deleteButton);
+    incompleteList.appendChild(liTag);
+};
+const buildTodoTag = (todoName, buttons)=>{
+    const li = document.createElement("li");
+    const div = document.createElement("div");
+    div.className = "list-row";
+    const p = document.createElement("p");
+    p.className = "todo-item";
+    p.innerText = todoName;
+    div.appendChild(p);
+    buttons.forEach((button)=>{
+        div.appendChild(button);
+    });
+    li.appendChild(div);
+    return li;
+};
+const createCompleteButton = ()=>{
+    return createButton("\u5B8C\u4E86", onClickCompleteButton);
+};
+const createDeleteButton = ()=>{
+    return createButton("\u524A\u9664", onClickDeleteButton);
+};
+const createIncompleteButton = ()=>{
+    return createButton("\u623B\u3059", onClickIncompleteButton);
+};
+const createButton = (value, event)=>{
+    const button = document.createElement("button");
+    button.innerText = value;
+    button.addEventListener("click", event);
+    return button;
+};
+document.getElementById("add-button").addEventListener("click", onClickAdd);
 
 },{"./styles.css":"lW6qc"}],"lW6qc":[function() {},{}]},["iZQ9B","aR1JP"], "aR1JP", "parcelRequirebaba")
 
